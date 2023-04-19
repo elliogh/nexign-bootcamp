3e868b96d0e65e1802bef671848e00a44514fcf9
danytse <73063926+danytse@users.noreply.github.com>
2023-04-19 19:35:29 +0300

    added hrs-service core

---
+++ hrs-service/src/main/java/ru/ellio/hrsservice/controller/HrsController.java
@@ -0,0 +1,9 @@
+package ru.ellio.hrsservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hrs")
+public class HrsController {
+}
