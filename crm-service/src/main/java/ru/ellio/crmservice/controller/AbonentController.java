a962b08506621102e4a9612a4b135430852098b6
danytse <73063926+danytse@users.noreply.github.com>
2023-04-20 18:04:06 +0300

    feat: crm-service add controllers core

--- crm-service/src/main/java/ru/ellio/crmservice/controller/AbonentController.java
+++ crm-service/src/main/java/ru/ellio/crmservice/controller/AbonentController.java
@@ -1,9 +1,22 @@
 package ru.ellio.crmservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/abonent")
 public class AbonentController {
+
+    @PatchMapping("/pay")
+    public String pay() {
+        return "";
+    }
+
+    @GetMapping("/report")
+    public String report() {
+        return "";
+    }
+
 }
--- crm-service/src/main/java/ru/ellio/crmservice/controller/ManagerController.java
+++ crm-service/src/main/java/ru/ellio/crmservice/controller/ManagerController.java
@@ -1,9 +1,26 @@
 package ru.ellio.crmservice.controller;
 
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/manager")
 public class ManagerController {
+
+    @PatchMapping("/chaneTariff")
+    public String changeTariff() {
+        return "";
+    }
+
+    @PostMapping("/abonent")
+    public String createAbonent() {
+        return "";
+    }
+
+    @PatchMapping("/billing")
+    public String billing() {
+        return "";
+    }
 }
