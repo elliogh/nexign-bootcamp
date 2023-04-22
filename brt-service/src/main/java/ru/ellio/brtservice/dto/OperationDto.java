dfbf85740e0ca2f58db15de294715a7ef959d731
danytse <73063926+danytse@users.noreply.github.com>
2023-04-22 22:46:18 +0300

    fix: remove json pattern in brt-service response

--- brt-service/src/main/java/ru/ellio/brtservice/dto/OperationDto.java
+++ brt-service/src/main/java/ru/ellio/brtservice/dto/OperationDto.java
@@ -1,10 +1,7 @@
 package ru.ellio.brtservice.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Data;
-import ru.ellio.brtservice.json.DurationSerializer;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -13,11 +10,8 @@ import java.time.LocalDateTime;
 @Builder
 public class OperationDto {
     private final String callType;
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime startTime;
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime endTime;
-    @JsonSerialize(using = DurationSerializer.class)
     private final Duration duration;
     private final double cost;
 }
