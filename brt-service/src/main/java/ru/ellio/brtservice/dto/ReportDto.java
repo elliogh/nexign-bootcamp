00ad2f00854502a0e64c2aa259e99f09dc1a4446
danytse <73063926+danytse@users.noreply.github.com>
2023-04-22 18:10:12 +0300

    fix: payload json bug in abonent/report

--- brt-service/src/main/java/ru/ellio/brtservice/dto/ReportDto.java
+++ brt-service/src/main/java/ru/ellio/brtservice/dto/ReportDto.java
@@ -1,5 +1,7 @@
 package ru.ellio.brtservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,4 +16,15 @@ public class ReportDto {
     private final List<OperationDto> payload;
     private final double totalCost;
     private final String monetaryUnit;
+
+    @JsonCreator
+    public ReportDto(long id, String numberPhone, String tariffIndex,
+                     @JsonProperty("numbers") List<OperationDto> payload, double totalCost, String monetaryUnit) {
+        this.id = id;
+        this.numberPhone = numberPhone;
+        this.tariffIndex = tariffIndex;
+        this.payload = payload;
+        this.totalCost = totalCost;
+        this.monetaryUnit = monetaryUnit;
+    }
 }
