db165f291aa5fb46b97039e0b6c2f52cb6afe6ea
danytse <73063926+danytse@users.noreply.github.com>
2023-04-22 22:30:25 +0300

    feat: add constraints to brt_db

--- brt-service/src/main/resources/db/migration/init_tables.sql
+++ brt-service/src/main/resources/db/migration/init_tables.sql
@@ -4,36 +4,36 @@ DROP TABLE IF EXISTS client;
 DROP TABLE IF EXISTS tariff;
 
 CREATE TABLE tariff(
-    id  BIGSERIAL PRIMARY KEY NOT NULL,
-    tariff_id   VARCHAR(32) NOT NULL,
-    name    VARCHAR(32) NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    default_minute_price  DOUBLE PRECISION NOT NULL,
-    tariff_minute_price   DOUBLE PRECISION NOT NULL,
-    minutes_left    INT NOT NULL,
-    incoming_free   BOOLEAN NOT NULL
+    id                      BIGSERIAL PRIMARY KEY NOT NULL,
+    tariff_id               VARCHAR(16) UNIQUE NOT NULL,
+    name                    VARCHAR(32) NOT NULL,
+    price                   DOUBLE PRECISION NOT NULL,
+    default_minute_price    DOUBLE PRECISION NOT NULL,
+    tariff_minute_price     DOUBLE PRECISION NOT NULL,
+    minutes_left            INT NOT NULL,
+    incoming_free           BOOLEAN NOT NULL
 );
 
 CREATE TABLE client(
-    id  BIGSERIAL PRIMARY KEY NOT NULL,
-    number_phone    VARCHAR(32) NOT NULL,
-    tariff_id   INT REFERENCES tariff NOT NULL,
-    balance DOUBLE PRECISION NOT NULL,
-    total_cost  FLOAT NOT NULL,
-    monetary_unit VARCHAR(32) NOT NULL
+    id                      BIGSERIAL PRIMARY KEY NOT NULL,
+    number_phone            VARCHAR(16) UNIQUE NOT NULL,
+    tariff_id               INT REFERENCES tariff NOT NULL,
+    balance                 DOUBLE PRECISION NOT NULL,
+    total_cost              FLOAT NOT NULL,
+    monetary_unit           VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE operation(
-    id  BIGSERIAL PRIMARY KEY,
-    call_type   VARCHAR(32) NOT NULL,
-    start_time  TIMESTAMP NOT NULL,
-    end_time  TIMESTAMP NOT NULL,
-    duration    BIGINT NOT NULL,
-    cost    DOUBLE PRECISION NOT NULL
+    id                      BIGSERIAL PRIMARY KEY,
+    call_type               VARCHAR(16) NOT NULL,
+    start_time              TIMESTAMP NOT NULL,
+    end_time                TIMESTAMP NOT NULL,
+    duration                BIGINT NOT NULL,
+    cost                    DOUBLE PRECISION NOT NULL
 );
 
 CREATE TABLE client_payload(
-    client_id  BIGINT NOT NULL REFERENCES client,
-    payload_id BIGINT NOT NULL REFERENCES operation
+    client_id               BIGINT NOT NULL REFERENCES client,
+    payload_id              BIGINT NOT NULL REFERENCES operation
 )
 
