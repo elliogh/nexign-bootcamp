4a99753791d1df8fd62fe1fc189e7dc3e4e971b0
danytse <73063926+danytse@users.noreply.github.com>
2023-04-22 18:33:53 +0300

    fix: date and duration in json

--- brt-service/src/main/java/ru/ellio/brtservice/dto/OperationDto.java
+++ brt-service/src/main/java/ru/ellio/brtservice/dto/OperationDto.java
@@ -1,7 +1,10 @@
 package ru.ellio.brtservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Data;
+import ru.ellio.brtservice.json.DurationSerializer;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -9,10 +12,12 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class OperationDto {
-    private final long id;
     private final String callType;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime startTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime endTime;
+    @JsonSerialize(using = DurationSerializer.class)
     private final Duration duration;
     private final double cost;
 }
---
+++ brt-service/src/main/java/ru/ellio/brtservice/json/DurationSerializer.java
@@ -0,0 +1,19 @@
+package ru.ellio.brtservice.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class DurationSerializer extends JsonSerializer<Duration> {
+    @Override
+    public void serialize(Duration duration, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String formattedDuration = String.format("%02d:%02d:%02d",
+                duration.toHours(),
+                duration.toMinutesPart(),
+                duration.toSecondsPart());
+        jsonGenerator.writeString(formattedDuration);
+    }
+}
